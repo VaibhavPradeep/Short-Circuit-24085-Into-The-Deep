1d7e5a574ea7d1af82c69eda6a3a9f15dd01630e
PSMV <Vaibhavpradeep777@gmail.com>
2024-11-03 09:28:13 -0500

    Changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrimBotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrimBotTeleOp.java
@@ -166,6 +166,6 @@ public class ScrimBotTeleOp extends OpMode {
         //telemetry.addData("Current Position", armSlide.getCurrentPosition());
         telemetry.update();
         telemetry.update();
-        // oi oko
+        // oi oko ojoj
     }
 }
